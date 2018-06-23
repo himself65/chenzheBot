4df890cfa168e12f311d8479e04ecbf4d853a22c
Himself65 <himself6565@gmail.com>
2018-06-24 00:10:06 +0800

    update

--- chenzheBot.py
+++ chenzheBot.py
@@ -6,9 +6,11 @@ from bot.CZBotWord import CZBotWord
 
 czbot = CZBotWord()
 group_name = ['hxr粉丝群', ]
-
+message_list = []
 
 def onQQMessage(bot, contact, member, content):
+    if len(message_list) > 10:
+        message_list.clear()
     if contact.ctype != 'group':
         # 忽略非群消息
         return
@@ -16,4 +18,7 @@ def onQQMessage(bot, contact, member, content):
         ans = czbot.getSentence(content)  # 得到回复内容，有可能为空
         if ans == '' or ans is None:
             return
-        bot.SendTo(contact, ans)
+        if ans not in message_list:
+            bot.SendTo(contact, ans)
+            message_list.append(ans)
+        
